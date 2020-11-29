f5e57ae7aebfa8be4ee3a3e439acb16ab4851d3e
SMN947 <betum98@gmail.com>
2020-11-29 15:03:13 -0500

    marker updated with position change, easybutton lib demo implemented

--- src/js/game.js
+++ src/js/game.js
@@ -1,24 +1,29 @@
 class Game {
   constructor(mapID) {
     console.log(mapID);
+
     this.map = L.map(mapID).setView([0, 0], 14);
-    console.log(this.map);
+    this.marker = L.marker([0, 0], {
+      draggable: true
+    }).addTo(this.map);
     this.renderMap();
   }
   renderMap() {
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
-      maxZoom: 18
+      maxZoom: 30
     }).addTo(this.map);
 
-    L.control.scale().addTo(this.map);
-    L.marker([0, 0], {
-      draggable: true
+
+
+    L.easyButton('<strong>1</strong>', function() {
+      this.disable();
     }).addTo(this.map);
+    L.control.scale().addTo(this.map);
   }
-  update(map, loc) {
-    console.log(map, loc);
-    map.flyTo([loc.coords.latitude, loc.coords.longitude], 18)
+  update(map, marker, loc) {
+    marker.setLatLng([loc.coords.latitude, loc.coords.longitude])
+    map.flyTo([loc.coords.latitude, loc.coords.longitude], 19)
   }
   services = {
     location: () => {
@@ -28,8 +33,9 @@ class Game {
       }
       var map = this.map;
       var update = this.update;
+      var marker = this.marker;
       navigator.geolocation.watchPosition((loc) => {
-        update(map, loc)
+        update(map, marker, loc)
       }, this.error, geoOptions);
 
     }
